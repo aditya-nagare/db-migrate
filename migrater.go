667e123b4e7e28108527fd4c34386dba2ecda1df
Pankaj Khairnar <pankaj.khairnar@gmail.com>
2016-02-10 08:48:00 +0530

    working on creating sqls folder and configuration file

--- migrater.go
+++ migrater.go
@@ -1,27 +1,40 @@
 package main
+//	"flag"
 
 import (
 	"fmt"
-	"flag"
 	"os"
+	"io/ioutil"
 )
 
 func main() {
-	initPtr := flag.Bool("init", false, "a string")
-	flag.Parse()	
+	 var initPtr = true
+//	initPtr := flag.Bool("init", false, "a string")
+//	flag.Parse()	
 	
-	if (*initPtr == true) {
-		folderExist, err   := exists("./sqls")
-		confFileExist, err := exists("./migrater.conf")
+	if (initPtr == true) {
+		var folderExist = false
+		var confFileExist = false
+//		folderExist, err   := exists("./sqls")
+//		confFileExist, err := exists("./migrater.conf")
+//		if(err == nil) {
+//			fmt.Println("Some error occured")
+//		}
 		
 		if(folderExist == false && confFileExist == false) { //folder, conf does not exist
 			fmt.Println("sqls AND conf does not exist -- creating")
+			createConfFile()
+			createSqlsFolder()
 		} else if(folderExist == true && confFileExist == false) {//folder exist, conf not
-			fmt.Println("Both sqls and conf exist")
-		} else if (folderExist == true && confFileExist == false) {
-			fmt.Println("SQL folder exist, conf does not exist create it")
+			fmt.Println("folder exist, file not")
+			createConfFile()
+		} else if (folderExist == false && confFileExist == true) {
+			createSqlsFolder()
+			fmt.Println("SQL folder not exist, conf does exist")
+		} else { //both exist
+			fmt.Println("Already an migrater directory")
 		}
-		fmt.Println("Value:", *initPtr)	
+//		fmt.Println("Value:", *initPtr)	
 	}
 }
 
@@ -40,10 +53,28 @@ func exists(path string) (bool, error) {
     return true, err
 }
 
+func createConfFile() (bool, error) {
+	var content = []byte("content configuration")
+	var err = ioutil.WriteFile("./settings.conf", content, 0755)
+    if err != nil {
+        panic(err)
+    }
+	return true, nil
+}
+
+func createSqlsFolder() (bool, error) {
+	var err = os.Mkdir("sqls", 0755)
+    if err != nil {
+        panic(err)
+    }
+	return true, nil
+}
 /**
 
-migrater -init : will create "sqls" directory, migrater.conf file 
-migrater -new : should be run from valid migrater directory, having "sqls" and migrater.conf file
+migrater -init : will create "sqls" directory, migrater.conf file it will have connection info for databas
+migrater -new : should be run from valid migrater directory, having "sqls" folder, it will create .sql 
+	file in sqls directory, will take first 50 chars of desc and append with name like 
+	0002_desc_of_file.sql, it will also write desc in file itself at start of file commented.
 migrater -update : will run all the migration against database
 migrater -v 5 : it will up or down the migrations from sqls folder
 how am I going to use conf 
